73f49b00ae0e6e80cb16e7bb631a63dc2b382302
Nadezhda Morozova <forever-nad@yandex.ru>
2023-06-02 15:45:08 +0300

    м2

--- element-search/popups/task.js
+++ element-search/popups/task.js
@@ -1,11 +1,11 @@
 const popup = document.querySelector("#modal_main");
 const popupSuccess = document.querySelector("#modal_success");
-const popupCloseBtn = document.querySelector(".modal__close");
+const popupCloseBtns = document.querySelectorAll(".modal__close");
 const popupCloseShowBtn = document.querySelector(".show-success");
 const activePopupClassName = "modal_active";
 
 popup.classList.add(activePopupClassName);
 
-popupCloseBtn.addEventListener("click", () => popup.classList.remove(activePopupClassName));
+popupCloseBtns.forEach((btn) => btn.addEventListener("click", (e) => e.target.closest(".modal").classList.remove(activePopupClassName)));
 
 popupCloseShowBtn.addEventListener("click", () => popupSuccess.classList.add(activePopupClassName));
